0fb273487d01f6b80cd9e02e569284d98a9f5caa
Nguyen Thanh Hieu <45602217+Neliem7198@users.noreply.github.com>
2019-11-28 22:47:48 +0700

    edit Muon.xaml , add page UpdateQuaTrinhMuon

---
+++ App CSDLPT/csdl-pt/Pages/UpdateQuaTrinhMuon.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace csdl_pt.Pages
+{
+    /// <summary>
+    /// Interaction logic for UpdateQuaTrinhMuon.xaml
+    /// </summary>
+    public partial class UpdateQuaTrinhMuon : Page
+    {
+        string connectionString;
+        public UpdateQuaTrinhMuon(string connectionString)
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BtnBack_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService navService = NavigationService.GetNavigationService(this);
+            Muon pg = new Muon(connectionString);
+            navService.Navigate(pg);
+        }
+
+        private void BtnUpdate_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
