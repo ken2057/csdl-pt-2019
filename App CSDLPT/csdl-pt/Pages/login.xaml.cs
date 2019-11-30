6dd1dc5f6e6aef3b1f419a84ff5bac387422c87b
Duy <zzxxcc2056@gmail.com>
2019-11-30 18:18:45 +0700

    add login with all sv

--- App CSDLPT/csdl-pt/Pages/login.xaml.cs
+++ App CSDLPT/csdl-pt/Pages/login.xaml.cs
@@ -55,6 +55,50 @@ namespace csdl_pt.Pages
             }
         }
 
+        private void login_to_all_sv()
+        {
+            bool isLogin = false;
+            List<string> listCS = new List<string>();
+
+            // get all connecitonString exist
+            listCS.Add(ConfigurationManager.ConnectionStrings["qltv_maychu"].ConnectionString);
+            listCS.Add(ConfigurationManager.ConnectionStrings["qltv_tram_1"].ConnectionString);
+            listCS.Add(ConfigurationManager.ConnectionStrings["qltv_tram_2"].ConnectionString);
+            listCS.Add(ConfigurationManager.ConnectionStrings["qltv_tram_3"].ConnectionString);
+            listCS.Add(ConfigurationManager.ConnectionStrings["qltv_tram_4"].ConnectionString);
+
+            // try login each of them
+            foreach (var cS in listCS)
+            {
+                string connectionString = cS + "User ID=" + txtUsername.Text + "; Password=" + txtPassword.Password;
+                using (var conn = new SqlConnection(connectionString))
+                {
+                    try
+                    {
+                        conn.Open();
+                        // open if login success
+                        NavigationService navService = NavigationService.GetNavigationService(this);
+                        showOption sO = new showOption(connectionString);
+                        navService.Navigate(sO);
+                        isLogin = true;
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        //MessageBox.Show(exception.ToString());
+                    }
+                    finally
+                    {
+                        conn.Close();
+                    }
+                }
+            }
+
+            // after finish, still not login => show error
+            if (!isLogin)
+                MessageBox.Show("Tài khoản hoặc mật khẩu không hợp lệ", 16, 1);
+        }
+
         private void gridLogin_KeyDown(object sender, KeyEventArgs e)
         {
             if(e.Key == Key.Enter)
